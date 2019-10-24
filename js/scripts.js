16f920cf7162dc8bb8ad294ef6349f7cade9b947
Trevor Mackin and Uriel Gonzalez <uriel@>
2019-10-24 11:37:23 -0700

    Add indentation fix.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-
   var usersHeight = parseInt(prompt("Please enter your height in inches: "));
-
-    if (usersHeight < 36) {
-      $("#ferris-id").removeClass();
-    }
-
-    if (usersHeight >= 36) {
-      $("#ferris-id").removeClass();
-      $("#roller-id").removeClass();
-    }
-    if (usersHeight >= 48) {
-      $("#ferris-id").removeClass();
-      $("#roller-id").removeClass();
-      $("#tower-id").removeClass();
-    }
-
+  if (usersHeight < 36) {
+    $("#ferris-id").removeClass();
+  }
+  if (usersHeight >= 36) {
+    $("#ferris-id").removeClass();
+    $("#roller-id").removeClass();
+  }
+  if (usersHeight >= 48) {
+    $("#ferris-id").removeClass();
+    $("#roller-id").removeClass();
+    $("#tower-id").removeClass();
+  }
 });
